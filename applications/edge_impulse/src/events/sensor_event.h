487e79161bba0425cffb54d1a638835b9e2ffba3
Marek Pieta <Marek.Pieta@nordicsemi.no>
2021-02-12 13:51:21 +0100

    applications: edge_impulse: Add sensor_event
    Change adds sensor_event.
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/edge_impulse/src/events/sensor_event.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+#ifndef _SENSOR_EVENT_H_
+#define _SENSOR_EVENT_H_
+
+/**
+ * @brief Sensor Event
+ * @defgroup sensor_event Sensor Event
+ * @{
+ */
+
+#include "event_manager.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct sensor_event {
+	struct event_header header;
+	const char *descr;
+
+	struct event_dyndata dyndata;
+};
+
+EVENT_TYPE_DYNDATA_DECLARE(sensor_event);
+
+static inline size_t sensor_event_get_data_cnt(const struct sensor_event *event)
+{
+	__ASSERT_NO_MSG((event->dyndata.size / sizeof(float)) == 0);
+
+	return (event->dyndata.size / sizeof(float));
+}
+
+static inline const float *sensor_event_get_data_ptr(const struct sensor_event *event)
+{
+	return (const float *)event->dyndata.data;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* _SENSOR_EVENT_H_ */
