dbf3a31a44017bfae63534cf8e725b03cc399176
Marek Pieta <Marek.Pieta@nordicsemi.no>
2021-03-11 17:50:08 +0100

    applications: machine_learning: Add NUS to EI data forwarder
    Change integrates forwarding sensor samples over NUS and adds basic
    Bluetooth configurations.
    
    Jira: NCSDK-8507
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- applications/machine_learning/src/modules/ei_data_forwarder.c
+++ applications/machine_learning/src/modules/ei_data_forwarder.c
@@ -7,8 +7,10 @@
 #include <zephyr.h>
 #include <stdio.h>
 #include <drivers/uart.h>
+#include <bluetooth/services/nus.h>
 
 #include <caf/events/sensor_event.h>
+#include <caf/events/ble_common_event.h>
 
 #define MODULE ei_data_forwarder
 #include <caf/events/module_state_event.h>
@@ -16,10 +18,27 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(MODULE, CONFIG_ML_APP_EI_DATA_FORWARDER_LOG_LEVEL);
 
-#define UART_LABEL		CONFIG_ML_APP_EI_DATA_FORWARDER_UART_DEV
+#if defined(CONFIG_ML_APP_EI_DATA_FORWARDER_UART)
+	#define UART_LABEL		CONFIG_ML_APP_EI_DATA_FORWARDER_UART_DEV
+#else
+	#define UART_LABEL		""
+#endif
+
 #define UART_BUF_SIZE		CONFIG_ML_APP_EI_DATA_FORWARDER_BUF_SIZE
 
+#define MIN_CONN_INT		CONFIG_BT_PERIPHERAL_PREF_MIN_INT
+#define MAX_CONN_INT		CONFIG_BT_PERIPHERAL_PREF_MAX_INT
+
+enum {
+	CONN_SECURED			= BIT(0),
+	CONN_INTERVAL_VALID		= BIT(1),
+	CONN_ERROR			= BIT(2),
+};
+
+static uint8_t conn_state;
+
 static const struct device *dev;
+static struct bt_conn *nus_conn;
 static atomic_t uart_busy;
 
 enum state {
@@ -48,10 +67,37 @@ static int snprintf_error_check(int res, size_t buf_size)
 	return 0;
 }
 
+static bool is_nus_conn_valid(struct bt_conn *conn, uint8_t conn_state)
+{
+	if (!conn) {
+		return false;
+	}
+
+	if (conn_state & CONN_ERROR) {
+		return false;
+	}
+
+	if (!(conn_state & CONN_INTERVAL_VALID)) {
+		return false;
+	}
+
+	if (!(conn_state & CONN_SECURED)) {
+		return false;
+	}
+
+	return true;
+}
+
 static bool handle_sensor_event(const struct sensor_event *event)
 {
 	if (state != STATE_ACTIVE) {
 		return false;
+	}
+
+	if (IS_ENABLED(CONFIG_ML_APP_EI_DATA_FORWARDER_BT_NUS)) {
+		if (!is_nus_conn_valid(nus_conn, conn_state)) {
+			return false;
+		}
 	}
 
 	__ASSERT_NO_MSG(sensor_event_get_data_cnt(event) > 0);
@@ -85,11 +131,27 @@ static bool handle_sensor_event(const struct sensor_event *event)
 		pos += tmp;
 	}
 
-	int err = uart_tx(dev, buf, pos, SYS_FOREVER_MS);
+	if (IS_ENABLED(CONFIG_ML_APP_EI_DATA_FORWARDER_UART)) {
+		int err = uart_tx(dev, buf, pos, SYS_FOREVER_MS);
 
-	if (err) {
-		LOG_ERR("uart_tx error: %d", err);
-		report_error();
+		if (err) {
+			LOG_ERR("uart_tx error: %d", err);
+			report_error();
+		}
+	} else if (IS_ENABLED(CONFIG_ML_APP_EI_DATA_FORWARDER_BT_NUS)) {
+		if (bt_nus_get_mtu(nus_conn) < pos) {
+			atomic_cas(&uart_busy, true, false);
+			LOG_WRN("GATT MTU too small");
+			return false;
+		}
+
+		int err = bt_nus_send(nus_conn, buf, pos);
+
+		if (err) {
+			LOG_ERR("bt_nus_tx error: %d", err);
+			atomic_cas(&uart_busy, true, false);
+                        //report_error();
+                }
 	}
 
 	return false;
@@ -103,7 +165,7 @@ static void uart_cb(const struct device *dev, struct uart_event *evt,
 	}
 }
 
-static int init(void)
+static int init_uart(void)
 {
 	dev = device_get_binding(UART_LABEL);
 
@@ -121,6 +183,104 @@ static int init(void)
 	return err;
 }
 
+void bt_nus_sent_cb(struct bt_conn *conn)
+{
+	atomic_cas(&uart_busy, true, false);
+}
+
+static struct bt_nus_cb nus_cb = {
+	.sent = bt_nus_sent_cb,
+};
+
+static int init_nus(void)
+{
+	int err = bt_nus_init(&nus_cb);
+
+	if (err) {
+		LOG_ERR("Cannot initialize NUS (err %d)", err);
+		report_error();
+	}
+
+	return err;
+}
+
+static bool handle_module_state_event(const struct module_state_event *event)
+{
+	int err = 0;
+
+	if (IS_ENABLED(CONFIG_ML_APP_EI_DATA_FORWARDER_UART) &&
+	    check_state(event, MODULE_ID(main), MODULE_STATE_READY)) {
+
+		__ASSERT_NO_MSG(state == STATE_DISABLED);
+		err = init_uart();
+
+		if (!err) {
+			state = STATE_ACTIVE;
+			module_set_state(MODULE_STATE_READY);
+		} else {
+			report_error();
+		}
+	} else if (IS_ENABLED(CONFIG_ML_APP_EI_DATA_FORWARDER_BT_NUS) &&
+		  check_state(event, MODULE_ID(ble_state), MODULE_STATE_READY)) {
+
+		__ASSERT_NO_MSG(state == STATE_DISABLED);
+		err = init_nus();
+
+		if (!err) {
+			state = STATE_ACTIVE;
+			module_set_state(MODULE_STATE_READY);
+		} else {
+			report_error();
+		}
+	}
+
+	return false;
+}
+
+static bool handle_ble_peer_event(const struct ble_peer_event *event)
+{
+	switch (event->state) {
+	case PEER_STATE_CONNECTED:
+		nus_conn = event->id;
+		break;
+
+	case PEER_STATE_SECURED:
+		__ASSERT_NO_MSG(nus_conn);
+		conn_state |= CONN_SECURED;
+		break;
+
+	case PEER_STATE_DISCONNECTED:
+	case PEER_STATE_DISCONNECTING:
+		conn_state &= ~CONN_SECURED;
+		nus_conn = NULL;
+		break;
+
+	default:
+		break;
+	}
+
+	return false;
+}
+
+static bool handle_ble_peer_conn_params_event(const struct ble_peer_conn_params_event *event)
+{
+	if (!event->updated) {
+		return false;
+	}
+
+	__ASSERT_NO_MSG(event->interval_min == event->interval_max);
+
+	uint16_t interval = event->interval_min;
+
+	if ((interval >= MIN_CONN_INT) && (interval <= MAX_CONN_INT)) {
+		conn_state |= CONN_INTERVAL_VALID;
+	} else {
+		conn_state &= ~CONN_INTERVAL_VALID;
+	}
+
+	return false;
+}
+
 static bool event_handler(const struct event_header *eh)
 {
 	if (is_sensor_event(eh)) {
@@ -128,20 +288,17 @@ static bool event_handler(const struct event_header *eh)
 	}
 
 	if (is_module_state_event(eh)) {
-		const struct module_state_event *event = cast_module_state_event(eh);
+		return handle_module_state_event(cast_module_state_event(eh));
+	}
 
-		if (check_state(event, MODULE_ID(main), MODULE_STATE_READY)) {
-			__ASSERT_NO_MSG(state == STATE_DISABLED);
+	if (IS_ENABLED(CONFIG_ML_APP_EI_DATA_FORWARDER_BT_NUS) &&
+	    is_ble_peer_event(eh)) {
+		return handle_ble_peer_event(cast_ble_peer_event(eh));
+	}
 
-			if (!init()) {
-				state = STATE_ACTIVE;
-				module_set_state(MODULE_STATE_READY);
-			} else {
-				report_error();
-			}
-		}
-
-		return false;
+	if (IS_ENABLED(CONFIG_ML_APP_EI_DATA_FORWARDER_BT_NUS) &&
+	    is_ble_peer_conn_params_event(eh)) {
+		return handle_ble_peer_conn_params_event(cast_ble_peer_conn_params_event(eh));
 	}
 
 	/* If event is unhandled, unsubscribe. */
@@ -153,3 +310,7 @@ static bool event_handler(const struct event_header *eh)
 EVENT_LISTENER(MODULE, event_handler);
 EVENT_SUBSCRIBE(MODULE, module_state_event);
 EVENT_SUBSCRIBE(MODULE, sensor_event);
+#ifdef CONFIG_ML_APP_EI_DATA_FORWARDER_BT_NUS
+EVENT_SUBSCRIBE(MODULE, ble_peer_event);
+EVENT_SUBSCRIBE(MODULE, ble_peer_conn_params_event);
+#endif /* CONFIG_ML_APP_EI_DATA_FORWARDER_BT_NUS */
