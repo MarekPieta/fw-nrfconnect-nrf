959ca34c5be2dfee74010f0c63e767ee1507ebe4
Marek Pieta <Marek.Pieta@nordicsemi.no>
2022-11-18 14:17:54 +0100

    bluetooth: adv_prov: Align Fast Pair payload with advertising state
    Change aligns Fast Pair payload with new information included in
    advertising state (advertising session start and RPA rotation).
    
    Jira: NCSDK-17509
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- subsys/bluetooth/adv_prov/providers/fast_pair.c
+++ subsys/bluetooth/adv_prov/providers/fast_pair.c
@@ -12,10 +12,18 @@
 static bool enabled = true;
 static bool show_ui_pairing = IS_ENABLED(CONFIG_BT_ADV_PROV_FAST_PAIR_SHOW_UI_PAIRING);
 
+static struct bt_data gen_ad;
+static enum bt_fast_pair_adv_mode gen_adv_mode = BT_FAST_PAIR_ADV_MODE_COUNT;
+
 
 void bt_le_adv_prov_fast_pair_enable(bool enable)
 {
 	enabled = enable;
+
+	if (!enabled) {
+		/* Invalidate generated payload. */
+		gen_adv_mode = BT_FAST_PAIR_ADV_MODE_COUNT;
+	}
 }
 
 void bt_le_adv_prov_fast_pair_show_ui_pairing(bool enable)
@@ -23,17 +31,11 @@ void bt_le_adv_prov_fast_pair_show_ui_pairing(bool enable)
 	show_ui_pairing = enable;
 }
 
-static int get_data(struct bt_data *ad, const struct bt_le_adv_prov_adv_state *state,
-		    struct bt_le_adv_prov_feedback *fb)
+static enum bt_fast_pair_adv_mode get_adv_mode(bool pairing_mode)
 {
-	static uint8_t buf[ADV_DATA_BUF_SIZE];
 	enum bt_fast_pair_adv_mode adv_mode;
 
-	if (!enabled) {
-		return -ENOENT;
-	}
-
-	if (state->pairing_mode) {
+	if (pairing_mode) {
 		adv_mode = BT_FAST_PAIR_ADV_MODE_DISCOVERABLE;
 	} else {
 		if (show_ui_pairing) {
@@ -43,14 +45,66 @@ static int get_data(struct bt_data *ad, const struct bt_le_adv_prov_adv_state *s
 		}
 	}
 
-	if (IS_ENABLED(CONFIG_BT_ADV_PROV_FAST_PAIR_AUTO_SET_PAIRING_MODE)) {
-		/* Set Fast Pair pairing mode to match Fast Pair advertising mode. */
-		bt_fast_pair_set_pairing_mode(adv_mode == BT_FAST_PAIR_ADV_MODE_DISCOVERABLE);
-	}
+	return adv_mode;
+}
+
+static int generate_payload(enum bt_fast_pair_adv_mode adv_mode)
+{
+	static uint8_t buf[ADV_DATA_BUF_SIZE];
 
 	__ASSERT_NO_MSG(bt_fast_pair_adv_data_size(adv_mode) <= sizeof(buf));
 
-	return bt_fast_pair_adv_data_fill(ad, buf, sizeof(buf), adv_mode);
+	int err = bt_fast_pair_adv_data_fill(&gen_ad, buf, sizeof(buf), adv_mode);
+
+	if (!err) {
+		gen_adv_mode = adv_mode;
+	} else {
+		gen_adv_mode = BT_FAST_PAIR_ADV_MODE_COUNT;
+	}
+
+	return err;
+}
+
+static int get_data(struct bt_data *ad, const struct bt_le_adv_prov_adv_state *state,
+		    struct bt_le_adv_prov_feedback *fb)
+{
+	enum bt_fast_pair_adv_mode expected_adv_mode = get_adv_mode(state->pairing_mode);
+	bool generate_new_payload = (expected_adv_mode != gen_adv_mode);
+	int err = 0;
+
+	if (!enabled) {
+		return -ENOENT;
+	}
+
+	if (IS_ENABLED(CONFIG_BT_ADV_PROV_FAST_PAIR_STOP_DISCOVERABLE_ON_RPA_ROTATION) &&
+	    (gen_adv_mode == BT_FAST_PAIR_ADV_MODE_DISCOVERABLE) &&
+	    state->rpa_rotated && !state->new_adv_session) {
+		LOG_WRN("RPA rotated during Fast Pair discoverable advertising session");
+		LOG_WRN("Removed Fast Pair advertising payload");
+		return -ENOENT;
+	}
+
+	if ((gen_adv_mode == BT_FAST_PAIR_ADV_MODE_NOT_DISCOVERABLE_SHOW_UI_IND) ||
+	    (gen_adv_mode == BT_FAST_PAIR_ADV_MODE_NOT_DISCOVERABLE_HIDE_UI_IND)) {
+		if (state->rpa_rotated) {
+			generate_new_payload = true;
+		}
+	}
+
+	if (generate_new_payload) {
+		err = generate_payload(expected_adv_mode);
+	}
+
+	if (!err) {
+		ad = gen_ad;
+		if (IS_ENABLED(CONFIG_BT_ADV_PROV_FAST_PAIR_AUTO_SET_PAIRING_MODE)) {
+			/* Set Fast Pair pairing mode to match Fast Pair advertising mode. */
+			bt_fast_pair_set_pairing_mode(expected_adv_mode ==
+						      BT_FAST_PAIR_ADV_MODE_DISCOVERABLE);
+		}
+	}
+
+	return err;
 }
 
 BT_LE_ADV_PROV_AD_PROVIDER_REGISTER(fast_pair, get_data);
