9f7e589e8c3f583f07a427254c9434234a639877
Marek Pieta <Marek.Pieta@nordicsemi.no>
2024-03-08 11:28:31 +0100

    mapi_fixes

--- samples/benchmarks/coremark/sysbuild.cmake
+++ samples/benchmarks/coremark/sysbuild.cmake
@@ -1,17 +1,10 @@
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-if(${images_added})
-  # same source directory is used for all targets
-  # to avoid recursive calls do not add project if remote was already added
-  return()
-endif()
 
 if(NOT SB_CONFIG_IMAGE_2_BOARD STREQUAL "")
 
-  list(INSERT IMAGES 0 coremark_image_2)
-
   ExternalZephyrProject_Add(
-    APPLICATION coremark_image_2
+    APPLICATION coremark_${SB_CONFIG_IMAGE_2_BOARD}
     SOURCE_DIR ${APP_DIR}
     BOARD ${SB_CONFIG_IMAGE_2_BOARD}
     BOARD_REVISION ${BOARD_REVISION}
@@ -32,4 +25,3 @@ if(NOT SB_CONFIG_IMAGE_3_BOARD STREQUAL "")
 
 endif()
 
-set(images_added 1)
