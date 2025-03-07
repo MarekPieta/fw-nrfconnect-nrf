ef2aba45f9c28657088eca102dc2f2e64b910312
Marek Pieta <Marek.Pieta@nordicsemi.no>
2025-03-07 16:29:52 +0100

    applications: nrf_desktop: Add keys state utility
    Change introduces keys state utility. The utility is used to track state
    of the pressed keys.
    
    Jira: NCSDK-31553
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/nrf_desktop/src/util/keys_state.c
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include "keys_state.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(keys_state, CONFIG_DESKTOP_KEYS_STATE_LOG_LEVEL);
+
+
+static bool keys_state_is_initialized(const struct keys_state *ks)
+{
+	return (ks->cnt_max > 0);
+}
+
+void keys_state_init(struct keys_state *ks, uint8_t key_cnt_max)
+{
+	ARG_UNUSED(keys_state_is_initialized);
+	__ASSERT_NO_MSG(!keys_state_is_initialized(ks));
+
+	__ASSERT_NO_MSG((key_cnt_max > 0) && (key_cnt_max <= ARRAY_SIZE(ks->keys)));
+	ks->cnt_max = key_cnt_max;
+}
+
+static int alloc_key(struct keys_state *ks, size_t idx, uint16_t key_id)
+{
+	/* Ensure proper key order. */
+	__ASSERT_NO_MSG(ks->keys[idx].id > key_id);
+	__ASSERT_NO_MSG((idx == 0) || (ks->keys[idx - 1].id < key_id));
+
+	__ASSERT_NO_MSG(ks->cnt <= ks->cnt_max);
+	if (ks->cnt == ks->cnt_max) {
+		LOG_WRN("No place on the list to store HID item! (%p)", (void *) ks);
+		return -ENOBUFS;
+	}
+
+	/* Shift active keys to make space for the new key. */
+	for (size_t k = ks->cnt; k > idx; k--) {
+		ks->keys[k] = ks->keys[k - 1];
+	}
+
+	ks->keys[idx].id = key_id;
+	ks->keys[idx].press_cnt = 0;
+
+	ks->cnt++;
+}
+
+static void free_key(struct keys_state *ks, size_t idx)
+{
+	/* Remove key and shift active keys. */
+	for (size_t k = idx; k < (ks->cnt - 1); k++) {
+		ks->keys[k] = ks->keys[k + 1];
+	}
+	ks->cnt--;
+
+	if (ks->keys[ks->cnt].press_cnt) {
+		ks->keys[ks->cnt].id = 0;
+		ks->keys[ks->cnt].press_cnt = 0;
+	}
+}
+
+static size_t get_key_idx(struct keys_state *ks, uint16_t key_id)
+{
+	for (size_t k = 0; k < ks->cnt; k++) {
+		if (ks->keys[k].id >= key_id) {
+			return k;
+		}
+	}
+
+	return ks->cnt;
+}
+
+int keys_state_key_update(struct keys_state *ks, uint16_t key_id, bool pressed, bool *keys_changed)
+{
+	__ASSERT_NO_MGS(keys_state_is_initialized(ks));
+
+	uint8_t prev_cnt = ks->cnt;
+	size_t key_idx = get_key_idx(ks, key_id);
+	struct active_key *key = ks->keys[key_idx];
+
+	if ((key->press_cnt == 0) || (key->id != key_id)) {
+		/* Key not yet tracked. Needs to be allocated. */
+		if (pressed) {
+			alloc_key(ks, key_idx, key_id);
+		} else {
+			/* Ignore release event for key that is not tracked. That could happen if
+			 * keys state is cleared while a button is kept pressed.
+			 */
+			key = NULL;
+		}
+	} else {
+		/* Key was already allocated. */
+		__ASSERT_NO_MSG(key->press_cnt > 0);
+	}
+
+	if (key) {
+		__ASSERT_NO_MSG(key->id == key_id);
+		__ASSERT_NO_MSG(pressed || (key->press_cnt > 0));
+		key->press_cnt += pressed ? (1) : (-1);
+
+		if (key->press_cnt == 0) {
+			free_key(ks, key_idx);
+		}
+	}
+
+	if (prev_cnt != ks->cnt) {
+		*keys_changed = true;
+	}
+
+	return 0;
+}
+
+void keys_state_clear(struct keys_state *ks);
+{
+	__ASSERT_NO_MGS(keys_state_is_initialized(ks));
+
+	LOG_INF("Clear keys state (%p)", (void *)ks);
+	memset(ks->keys, 0, sizeof(ks->keys));
+	ks->cnt = 0;
+}
+
+size_t keys_state_keys_get(const struct keys_state *ks, uint16_t *keys, size_t keys_size)
+{
+	__ASSERT_NO_MGS(keys_state_is_initialized(ks));
+	__ASSERT_NO_MSG(ks->cnt <= ks->cnt_max);
+
+	__ASSERT_NO_MSG(keys_size >= ks->cnt_max);
+	ARG_UNUSED(keys_size);
+
+	for (size_t i = 0; i < ks->cnt; i++) {
+		keys[i] = ks->keys[i].id;
+	}
+
+	return ks->cnt;
+}
---
+++ applications/nrf_desktop/src/util/keys_state.h
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#ifndef _KEYS_STATE_H_
+#define _KEYS_STATE_H_
+
+/**
+ * @file
+ * @defgroup keys_state Keys state
+ * @{
+ * @brief Utility used to track state of active keys.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#define KEYS_MAX_CNT	CONFIG_DESKTOP_KEYS_STATE_KEY_CNT_MAX
+
+/** @brief Structure used to track an active key. */
+struct active_key {
+	uint16_t id; /**< Key ID. */
+	uint16_t press_cnt; /**< Keypress counter. */
+};
+
+/**@brief Keys state structure. */
+struct keys_state {
+	struct active_key keys[KEYS_MAX_CNT]; /**< Active keys. */
+	uint8_t cnt; /**< Current number of active keys. */
+	uint8_t cnt_max; /**< Maximum numer of active keys. */
+};
+
+/**
+ * @brief Initialize a keys state object.
+ *
+ * A keys state object must be initialized before use.
+ *
+ * @param[out] ks		A keys state object.
+ * @param[in] key_cnt_max	Maximum number of active keys.
+ */
+void keys_state_init(struct hid_keys_state *ks, uint8_t key_cnt_max);
+
+/**
+ * @brief Notify keys state about a key press/release
+ *
+ * The function is called to notify the keys state that a key was pressed or released.
+ *
+ * @param[out] ks		A keys state object.
+ * @param[in] key_id		Key ID.
+ * @param[in] pressed		Information if key was pressed or released.
+ * @param[out] keys_changed	The boolean under pointer is set to true if keys state changed.
+ *
+ * @retval 0 on success.
+ * @retval -ENOBUFS if number of currently active keys exceeds the maximum number of active keys.
+ */
+int keys_state_key_update(struct keys_state *ks, uint16_t key_id, bool pressed, bool *keys_changed);
+
+/**
+ * @brief Clear keys state
+ *
+ * The function removes all of the tracked active key presses.
+ *
+ * @param[out] ks		A keys state object.
+ */
+void keys_state_clear(struct hid_keys_state *ks);
+
+/**
+ * @brief Get keys state
+ *
+ * The function fills the provided array with key IDs of all the active keys. The utility keeps key
+ * IDs sorted in ascending order to ensure consistent results.
+ *
+ * The function asserts if size of the provided array is too small to handle the worst case.
+ *
+ * @param[in] ks	A keys state object.
+ * @param[out] keys	Array to be filled with key IDs of active keys.
+ * @param[in] keys_size	Size of the provided array.
+ *
+ * @return Number of keys written.
+ */
+size_t keys_state_keys_get(const struct keys_state *ks, uint16_t *keys, size_t keys_size)
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /*_KEYS_STATE_H_ */
