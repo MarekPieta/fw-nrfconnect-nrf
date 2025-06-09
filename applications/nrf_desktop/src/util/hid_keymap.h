11834c959af6d9d0843cb1619ce23665927c2e48
Marek Pieta <Marek.Pieta@nordicsemi.no>
2025-06-09 20:51:56 +0200

    applications: nrf_desktop: Introduce HID keymap utility
    Change introduces utility that can be used to map app-specific key IDs
    to HID usage IDs. Change also integrates the utility in HID state module
    and application configurations.
    
    Jira: NCSDK-33877
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

---
+++ applications/nrf_desktop/src/util/hid_keymap.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ * @brief HID keymap header.
+ */
+
+#ifndef _HID_KEYMAP_H_
+#define _HID_KEYMAP_H_
+
+/**
+ * @defgroup hid_keymap HID keymap
+ * @brief Utility that handles mapping key IDs to HID usage IDs.
+ *
+ * @{
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include "hid_report_desc.h"
+
+/** @brief HID keymap entry. */
+struct hid_keymap {
+	uint16_t key_id;	/**< Application-specific key ID. */
+	uint16_t usage_id;	/**< Assigned HID usage ID. */
+	uint8_t report_id;	/**< Assigned HID report ID. */
+};
+
+/**
+ * @brief Initialize HID keymap
+ *
+ * The function initializes global data of HID keymap utility.
+ *
+ * The function must be called before using other HID keymap APIs. The function can be called
+ * multiple times.
+ */
+void hid_keymap_init(void);
+
+/**
+ * @brief Get mapping from key ID to HID usage ID.
+ *
+ * The function maps the application-specific key ID to HID keymap entry containing HID report ID
+ * and HID usage ID.
+ *
+ * @param[in] key_id		Application-specific key ID.
+ *
+ * @return HID keymap entry if mapping for the key ID is defined, NULL otherwise.
+ */
+const struct hid_keymap *hid_keymap_get(uint16_t key_id);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /*_HID_KEYMAP_H_ */
